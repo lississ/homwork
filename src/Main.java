ffa4076d35d133860d6722448572dd36bebf26df
Варвара <barbaraanaskina3@gmail.com>
2025-02-02 21:22:33 +0300

    Добавлен новый текст

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Доброе времени суток, Наставник!!");
+        System.out.println("Это моя первая домашка");
     }
 }
