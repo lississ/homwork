d3d6e260b227228b0032c146fe13e9f44186963c
Варвара <barbaraanaskina3@gmail.com>
2025-03-08 22:36:33 +0300

    Домашнее задание номер 4

--- src/Main.java
+++ src/Main.java
@@ -266,5 +266,91 @@ public class Main {
             max = three3;
         }
         System.out.println("Наибольшее число: " + max);
-                                            }
+
+        System.out.println("Домашнее задание 4");
+
+        System.out.println("Задача 1");
+        int clientOS = 0 ; // 0 — iOS, 1 — Android
+        if (clientOS == 0) {
+           System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        System.out.println("Задача 2");
+        int deviceYear = 2014; // Год выпуска устройства
+        if (deviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
+        System.out.println("Задача 3");
+        int year = 2022;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ){
+            System.out.println( year + " год является високосным");
+        }
+        else {
+            System.out.println( year + " год не является високосным");
+        }
+
+        System.out.println("Задача 4");
+        int deliveryDistance = 95; // дистанция до клиента
+        int deliveryTime;
+        if (deliveryDistance <= 20) {
+            deliveryTime = 1;
+        } else if (deliveryDistance <= 60) {
+            deliveryTime = 2;
+        } else if (deliveryDistance <= 100) {
+            deliveryTime = 3;
+        } else {
+            deliveryTime = 0;
+            System.out.println("Доставка невозможна");
+        }
+        if (deliveryTime > 0) {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        }
+
+        System.out.println("Задача 5");
+        int monthNumber = 12; // Номер месяца
+
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Некорректный номер месяца");
+        } else {
+            switch (monthNumber) {
+                case 12:
+                case 1:
+                case 2:
+                    System.out.println("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень");
+                    break;
+                default:
+                    System.out.println("Некорректный номер месяца");
+                    break;
+
+            }
+        }
+    }
                     }
