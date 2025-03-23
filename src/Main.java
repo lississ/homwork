e10c162a54c3380faf0d854b29efbccb64a363e8
Варвара <barbaraanaskina3@gmail.com>
2025-03-23 13:58:56 +0300

    Домашнее задание номер 6

--- src/Main.java
+++ src/Main.java
@@ -337,7 +337,7 @@ public class Main {
 
             }
         }
-        System.out.println( " Домашнее задание 5 ");
+        System.out.println( " Домашнее задание 5");
 
         System.out.println("Задача 1");
         for ( int i = 0; i <= 10; i++){
